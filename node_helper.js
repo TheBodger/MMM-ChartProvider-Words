f5003d5942b0c3d7e7895f2b510c4f4e54ce7e00
TheBodger <41484656+TheBodger@users.noreply.github.com>
2025-04-02 15:05:35 +0100

    Update node_helper.js / added punctuation removal from words

--- node_helper.js
+++ node_helper.js
@@ -10,6 +10,9 @@
 const moduleruntime = new Date();
 const stopwords = ["", "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shant", "she", "shed", "shell", "shes", "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why", "whys", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves"];
 
+const punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+const spaceRE = /\s+/g;
+
 //this loads and processes feeds into NDTF items of the count of words in the feeds, depending on its config when called to from the main module
 //
 //it must only send the same data once, 
@@ -363,9 +366,13 @@ module.exports = NodeHelper.create({
 
 			var thisword = wordarray[idx].toLowerCase();
 
+			//remove any punctuation
+
+			thisword = thisword.replace(punctRE, '').replace(spaceRE, ' ');
+
 			if (stopwords.indexOf(thisword) == -1 && thisword.length>2 && isNaN(parseInt(thisword))) {
 
-				if (wordlist[thisword] == null) {//need something to ignore useless words here
+				if (wordlist[thisword] == null) {
 					var tempitem = new structures.NDTFItem()
 					tempitem.object = feed.feedconfig.object;
 					tempitem.subject = thisword;
